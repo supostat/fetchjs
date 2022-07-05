dd40d9cd27b39ba522072e43cef072e9ac372064
Igor Pugachev <ipugachev84@gmail.com>
2022-07-05 20:03:25 +0100

    Add fetchjs

---
+++ fetchjs.js
@@ -0,0 +1,40 @@
+const isObject = (obj) => {
+  return Object.prototype.toString.call(obj) === "[object Object]";
+};
+
+let originalTarget;
+let propertiesChain = "";
+
+export function oFetch(target) {
+  if (!originalTarget) {
+    originalTarget = target;
+  }
+  if (target === undefined) {
+    showError("Object argument passed into fetchjs is undefined");
+  }
+
+  function showError(message) {
+    console.error("Error fetching from this object:", originalTarget);
+    throw new Error(message);
+  }
+
+  return new Proxy(target, {
+    get(target, prop) {
+      console.log(prop);
+      if (propertiesChain === "") {
+        propertiesChain += prop;
+      } else {
+        propertiesChain += `.${prop}`;
+      }
+      if (prop in target) {
+        if (isObject(target[prop])) {
+          return oFetch(target[prop]);
+        }
+        propertiesChain = "";
+        return target[prop];
+      } else {
+        showError(`Property '${propertiesChain}' is undefined`);
+      }
+    }
+  });
+};
